5ebc4dcbda2377e99b32b8b9719e180086dfa9c7
cory-eheart-1 <cory.eheart@gmail.com>
2025-10-24 14:17:35 -0500

    added a new title_card to home page, attempted to add tiktok snap scrolling

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
-
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Gracelyn Isabel Photography");
+  }
 }
